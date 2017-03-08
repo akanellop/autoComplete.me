0d7824ac9b637385fe34bedcfb5c18f6536cd52f
akanellop <akanellop42@gmail.com>
2017-03-08 19:53:40 +0200

    FOR BRANCH USE

--- DictNode.java
+++ DictNode.java
@@ -10,11 +10,32 @@ public class DictNode{
 		isTerminal=isEND;
 		capsType=caps;
 	}*/
+	public DictNode(){
+	}
 
-		public DictNode(DictNode [] node, boolean isEND, wordType caps){
+	public void NodeSetUp(DictNode [] node, boolean isEND, wordType caps){
 		newNode=node;
 		isTerminal=isEND;
 		capsType=caps;
 	}
+	
 
+	public static void main (String [] args) { 
+		DictNode [] temp = new DictNode[26];
+		DictNode [] root = new DictNode[26];
+		//temp= new DictNode(NULL,false,NOCAPS);
+		
+		try{
+		
+		//for(DictNode i : temp){
+			temp[].newNode= new DictNode[26];
+			temp[].NodeSetUp(root,false,wordType.NOCAPS);
+			System.out.println("jfk");
+		//}
+		
+		}
+		catch(Exception ex){ //error log
+			ex.printStackTrace();
+		}
+	}
 }
