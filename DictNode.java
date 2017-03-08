de22f68644f6f1c205379d16b9f7d7168b75f96e
Eleftherios P. Loukas <eleftheriosloukas@gmail.com>
2017-03-08 23:37:51 +0200

    tree iters, constructor DONE, class variables DONE!
    special thanks to @kkanellis

--- DictNode.java
+++ DictNode.java
@@ -1,32 +1,43 @@
 enum wordType { ALLCAPS, FIRSTCAP, NOCAPS }; //enum for CAPS info for the words
 
 public class DictNode{
-	public DictNode pointers[];
+	public DictNode pointers[];// = new DictNode[26];
 	public boolean isTerminal;
 	public wordType capsType=wordType.NOCAPS;
 
 	public DictNode(boolean isEND, wordType caps){ //constructor for object for DictNode class
-		DictNode [] pointers = new DictNode[26];//creates an array of DictNodes to be the first field of "root"oject (first 26 pointers)
+		pointers = new DictNode[26];//creates an array of DictNodes to be the first field of "root"oject (first 26 pointers)
 		isTerminal=isEND;
 		capsType=caps;
 	}
+	
 
 
 	public static void main (String [] args) { 
 		DictNode root = new DictNode(false,wordType.NOCAPS); // creates root of the tree
-		DictNode temp = new DictNode(false,wordType.NOCAPS);
+		DictNode next = new DictNode(false,wordType.NOCAPS);
+		DictNode next2 = new DictNode(false,wordType.NOCAPS);
 		
 		try{
+				
+			root.pointers[0] = new DictNode(false,wordType.NOCAPS);
+			//each DictNode should be called with its instructor
+			
+			next=root.pointers[0];
+			next2= next.pointers[25];
 			
 			
-		
-			if(root.pointers == null){
-				System.out.println("jfk");
+			if ( next == null) {
+				System.out.println("\n\nnext = null\n\n");
 			}
-			if(root == null){
-				System.out.println("jfk22");
+			
+			if ( next2 == null) {
+				System.out.println("\n\nnext2 = null");
 			}
-		
+			/*a = root.pointers[x]
+			a = a.pointers[y]
+			a = a.pointers[z]*/
+
 		}
 		catch(Exception ex){ //error log
 			ex.printStackTrace();
