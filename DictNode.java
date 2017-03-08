bb7be224757d74f73349217f6dec1a88a9ec23db
akanellop <akanellop42@gmail.com>
2017-03-08 20:34:39 +0200

    good construcotr and good null checks

--- DictNode.java
+++ DictNode.java
@@ -1,37 +1,31 @@
 enum wordType { ALLCAPS, FIRSTCAP, NOCAPS }; //enum for CAPS info for the words
 
 public class DictNode{
-	DictNode newNode[];
-	boolean isTerminal;
-	wordType capsType=wordType.NOCAPS;
+	public DictNode pointers[];
+	public boolean isTerminal;
+	public wordType capsType=wordType.NOCAPS;
 
-	/*public DictNode(int pos, boolean isEND, wordType caps){
-		newNode[pos]=new DictNode[];
-		isTerminal=isEND;
-		capsType=caps;
-	}*/
-	public DictNode(){
-	}
-
-	public void NodeSetUp(DictNode [] node, boolean isEND, wordType caps){
-		newNode=node;
+	public DictNode(boolean isEND, wordType caps){ //constructor for object for DictNode class
+		DictNode [] pointers = new DictNode[26];//creates an array of DictNodes to be the first field of "root"oject (first 26 pointers)
 		isTerminal=isEND;
 		capsType=caps;
 	}
-	
+
 
 	public static void main (String [] args) { 
-		DictNode [] temp = new DictNode[26];
-		DictNode [] root = new DictNode[26];
-		//temp= new DictNode(NULL,false,NOCAPS);
+		DictNode root = new DictNode(false,wordType.NOCAPS); // creates root of the tree
+		DictNode temp = new DictNode(false,wordType.NOCAPS);
 		
 		try{
+			
+			
 		
-		//for(DictNode i : temp){
-			temp[].newNode= new DictNode[26];
-			temp[].NodeSetUp(root,false,wordType.NOCAPS);
-			System.out.println("jfk");
-		//}
+			if(root.pointers == null){
+				System.out.println("jfk");
+			}
+			if(root == null){
+				System.out.println("jfk22");
+			}
 		
 		}
 		catch(Exception ex){ //error log
